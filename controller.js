d51a684fb8e086fce82a70164e41031a5b7111d5
quanto <quanto@users.noreply.github.com>
2014-03-05 16:19:03 +0100

    Update controller.js

--- controller.js
+++ controller.js
@@ -7,12 +7,15 @@ app.controller('HomeController', function($scope, $timeout, localStorageService)
 
     $scope.init = function(){
 
-        $.getJSON('json.php', function(data) {
-            $scope.data = data;
-            $scope.team = localStorageService.get('team')||data.team[0];
-
-            $scope.$apply();
-        });
+	var q= "select%20*%20from%20html%20where%20url%3D%22ado.kevinverhoef.nl%2Fjson.php%22%20and%20xpath%3D%27%2Fhtml%27";
+	$.getJSON("http://query.yahooapis.com/v1/public/yql?format=json&diagnostics=true&q=" + q,
+		
+		function(data) {
+			$scope.data = JSON.parse(decodeURI(JSON.stringify(data.query)));
+			$scope.team = localStorageService.get('team')||data.team[0];
+			$scope.$apply();
+		}
+	);
     }
 
     $scope.$watch('team', function(newValue, oldValue, scope) {
